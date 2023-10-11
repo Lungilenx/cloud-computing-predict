769badcf89b84d044256026a895331e045428448
Lungilenx <44583546+Lungilenx@users.noreply.github.com>
2023-10-11 17:56:52 +0200

    contact

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "{https://h23zn601wh.execute-api.eu-west-2.amazonaws.com/default/lamb}",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
