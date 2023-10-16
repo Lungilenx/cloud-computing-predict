3bbc96d117befd81accc29230ed0d8241f507cfc
Lungilenx <44583546+Lungilenx@users.noreply.github.com>
2023-10-16 14:46:15 +0200

    contact change

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -21,7 +21,7 @@ $(function () {
             }
             $this = $("#sendMessageButton");
             $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
-            var json = {name: lungile, phone: 0837729926, email: lungilenx@gmail.com, message: Hi}
+            var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
                 url: "{https://k537qarffa.execute-api.eu-west-2.amazonaws.com/default/LungileLM}",
