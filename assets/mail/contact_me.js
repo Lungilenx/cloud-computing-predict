561e7c8b1595e7bcfb64967cb7700b1a9a1f4b6b
Lungilenx <44583546+Lungilenx@users.noreply.github.com>
2023-10-20 10:44:09 +0200

    contact change

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -25,7 +25,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{https://f29w46o324.execute-api.eu-west-2.amazonaws.com}",
+                url: "{https://eu-west-2.console.aws.amazon.com/apigateway/main/api-detail?api=f29w46o324&region=eu-west-2}",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
